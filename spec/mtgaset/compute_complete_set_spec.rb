2b0cf790c75ea05866b217f7524a9a83b8cf8d69
Ren <renlester@myfave.com>
2020-05-29 14:53:28 +0800

    fixup! :sparkles: add compute complete set

--- spec/mtgaset/compute_complete_set_spec.rb
+++ spec/mtgaset/compute_complete_set_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe MTGASet::ComputeCompleteSet do
         number_of_packs_you_own: 5,
         average_number_of_rares_draft: 8,
         average_number_of_packs_draft: 1
-      ).call).to eq 19
+      ).call
+    ).to eq 20
   end
 end
